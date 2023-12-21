e93e99117b44d34e17cd610616659e2b23f507de
Justine Ballares <ballaresjustine@gmail.com>
2023-12-21 11:01:11 +0800

    Add RangeSlider component

---
+++ src/components/RangeSlider/index.tsx
@@ -0,0 +1,49 @@
+import React, { ChangeEvent } from 'react';
+
+interface RangeSliderProps {
+  id: string;
+  label: string;
+  value: number;
+  min: number;
+  max: number;
+  step: number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const RangeSlider: React.FC<RangeSliderProps> = ({
+  id,
+  label,
+  value,
+  min,
+  max,
+  step,
+  onChange,
+}) => {
+  return (
+    <>
+      <div className="flex flex-row mb-2">
+        <label
+          htmlFor={id}
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          {label}
+        </label>
+        <span className="ml-auto text-sm font-medium text-gray-900 dark:text-white">
+          {value}
+        </span>
+      </div>
+      <input
+        id={id}
+        type="range"
+        value={value}
+        min={min}
+        max={max}
+        step={step}
+        className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+        onChange={onChange}
+      />
+    </>
+  );
+};
+
+export default RangeSlider;
